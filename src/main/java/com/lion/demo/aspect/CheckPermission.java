7d254908f483094ddd49b63e5ee09128eb497166
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2024-12-04 15:28:25 +0900

    커스텀AOP로 간단한 권한 구현

---
+++ src/main/java/com/lion/demo/aspect/CheckPermission.java
@@ -0,0 +1,13 @@
+package com.lion.demo.aspect;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface CheckPermission {
+    String value(); // 필요한 권한 정의
+
+}
