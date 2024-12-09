3169af8cdbecf493dd70478b759611ead8fbeb63
DESKTOP-RENTAL\user <tkdduq241@naver.com>
2024-12-09 10:36:27 +0900

    @Value("${server.port}") ,          ResponseEntity<String> response = restTemplate.getForEntity(url,String.class);

---
+++ src/main/java/com/lion/demo/misc/MiscController.java
@@ -0,0 +1,27 @@
+package com.lion.demo.misc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/misc")
+public class MiscController {
+
+    @Autowired private ApiService apiService;
+    @Value("${server.port}") private String serverPort;
+
+    @GetMapping("/api")
+    public String api() {
+        String result = apiService.fetchData();
+        return result;
+    }
+
+    @GetMapping("/port")
+    public String port() {
+        return "server port = " + serverPort;
+    }
+
+}
