9e986d215927b008a5cfba2348c60e18d3744c61
DESKTOP-RENTAL\user <tkdduq241@naver.com>
2024-12-09 18:01:51 +0900

    ..

--- src/main/java/com/lion/demo/websocket/PersonalWebSocketHandler.java
+++ src/main/java/com/lion/demo/websocket/PersonalWebSocketHandler.java
@@ -31,7 +31,7 @@ public class PersonalWebSocketHandler extends TextWebSocketHandler {
         String userId = getUserId(session);
         String payload = message.getPayload(); // recipientId : message
         String[] parts = payload.split(":",2);
-        System.out.println("Recieved message: " + payload);
+//        System.out.println("Recieved message: " + payload);
 
         if (parts.length == 2) {
             String recipientId = parts[0];
