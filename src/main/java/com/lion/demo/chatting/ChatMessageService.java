2838159dba8b06bd232a78bbb7d2f9632a585489
DESKTOP-RENTAL\user <tkdduq241@naver.com>
2024-12-10 11:15:22 +0900

    개인톡관련 mock데이터 생성로직들

---
+++ src/main/java/com/lion/demo/chatting/ChatMessageService.java
@@ -0,0 +1,8 @@
+package com.lion.demo.chatting;
+
+import java.util.List;
+
+public interface ChatMessageService {
+    List<ChatMessage> getListByUser(String uid, String friendUid);
+    ChatMessage insertChatMessage(ChatMessage chatMessage);
+}
